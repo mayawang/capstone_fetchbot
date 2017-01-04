1bcfe31cf551d5b12710f074a249a209200094a8
Maya Wang <salome.neptune@gmail.com>
2017-01-03 21:55:03 -0800

    Remove generated files

--- frontend/app/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var app_module_1 = require("./app.module");
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
-// import { ContentStore } from './content-store..component';
-// bootstrap(ContentList, [ContentStore]);
-//# sourceMappingURL=main.js.map
