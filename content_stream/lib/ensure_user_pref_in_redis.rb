f281893243b145b706713ade64aabd88f646b8cc
Maya Wang <salome.neptune@gmail.com>
2017-01-26 01:19:26 -0800

    improve script speed

--- content_stream/lib/ensure_user_pref_in_redis.rb
+++ content_stream/lib/ensure_user_pref_in_redis.rb
@@ -1,5 +1,26 @@
 require 'recommendation_wrapper'
 
+USER_MAP = {}
+
+def save_like(user_id, article_id)
+  article = Article.find_by_id(article_id)
+  user = USER_MAP[user_id] || User.find_by_id(user_id)
+
+  unless user
+    puts "NO user found for id #{user_id}"
+    return
+  end
+
+  USER_MAP[user_id] = user
+
+  unless article
+    puts "NO article found for id #{article_id}"
+    return
+  end
+
+  user.like(article)
+end
+
 Content.all.each do |content|
   user_id = content.user_id
   article_id = content.article_id
@@ -15,9 +36,16 @@ Content.all.each do |content|
   end
 
   if content.rates == Content::RATE_LIKE
-    RecommendationApiWrapper.like(article_id, user_id)
+    save_like(user_id, article_id)
     puts "content liked: user #{user_id} article #{article_id}"
   else
     puts "content ignored: user #{user_id} article #{article_id} rates: #{content.rates}"
   end
+end
+
+User.find_each do |user|
+  puts "user: #{user.id}"
+  Recommendable::Helpers::Calculations.update_similarities_for(user.id.to_s)
+  Recommendable::Helpers::Calculations.update_recommendations_for(user.id.to_s)
+  puts "recommended_articles:#{user.recommended_articles.inspect}"
 end
