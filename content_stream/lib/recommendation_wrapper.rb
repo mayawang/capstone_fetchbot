97e034087f35fc75dde79fa56e1f42938b5abae0
Maya Wang <salome.neptune@gmail.com>
2017-01-26 21:31:25 -0800

    Ensure 3 text search results

--- content_stream/lib/recommendation_wrapper.rb
+++ content_stream/lib/recommendation_wrapper.rb
@@ -1,6 +1,18 @@
 require 'httparty'
 
 class RecommendationApiWrapper
+  ARTICLE_LIMIT = 3
+
+  def self.accumulate_articles(article_hash, new_articles, limit)
+    new_articles.each do |article|
+      if article_hash.keys.length >= limit
+        return
+      end
+
+      article_hash[article.id] = article
+    end
+  end
+
   def self.get_recommendation(user_id, query)
     user = User.find_by_id(user_id)
     unless user
@@ -8,96 +20,50 @@ class RecommendationApiWrapper
       return []
     end
 
+    found_articles = {}
     title_matched = Article.where('title LIKE ?', "%#{query}%")
-    if title_matched.count != 0
-      return title_matched[0,3]
+
+    self.accumulate_articles(found_articles, title_matched, ARTICLE_LIMIT)
+    if found_articles.keys.length >= ARTICLE_LIMIT
+      return found_articles.values[0, ARTICLE_LIMIT]
     end
 
-    summary_matched = Article.where('title LIKE ?', "%#{query}%")
-    if summary_matched.count != 0
-      return summary_matched[0,3]
+    summary_matched = Article.where('summary LIKE ?', "%#{query}%")
+    self.accumulate_articles(found_articles, summary_matched, ARTICLE_LIMIT)
+    if found_articles.keys.length >= ARTICLE_LIMIT
+      return found_articles.values[0, ARTICLE_LIMIT]
     end
 
     text_matched = Article.where('text LIKE ?', "%#{query}%")
-    if text_matched.count != 0
-      return text_matched[0,3]
+    self.accumulate_articles(found_articles, text_matched, ARTICLE_LIMIT)
+    if found_articles.keys.length >= ARTICLE_LIMIT
+      return found_articles.values[0, ARTICLE_LIMIT]
     end
 
-    puts "NO RECOMMENDATION!!!! return an normal recommandation article"
-    # if no recommandation, return based on keywords search
+    puts "NO SEARCH RESULTS!!!! return recommandation articles"
+    # if no text search results, try our luck with recommendations
 
     recommended = user.recommended_articles
-    if recommended.length > 0
-      return recommended
+    self.accumulate_articles(found_articles, recommended, ARTICLE_LIMIT)
+    if found_articles.keys.length >= ARTICLE_LIMIT
+      return found_articles.values[0, ARTICLE_LIMIT]
     end
 
+    attempts = 0
+    while found_articles.keys.length < ARTICLE_LIMIT
+      offset = rand(Article.count)
+      rand_article = Article.offset(offset).first
+      self.accumulate_articles(found_articles, [rand_article], ARTICLE_LIMIT)
 
-    # offset = rand(Article.count)
-    # return [
-    #   Article.offset(offset).first
-    # ]
+      if attempts > 2 * ARTICLE_LIMIT
+        puts "After #{attempts} attempts, still cannot find enough articles, returning"
+        break
+      end
+      attempts += 1
+    end
+
+    return found_articles.values[0, ARTICLE_LIMIT]
   end
-=begin
-    items = []
-    if "asimov laws of robotics".include?(query)
-      item_1 = {
-        id: '1',
-        title: "Do We Need Asimov's Laws - MIT Technology Review",
-        link: 'https://www.technologyreview.com/s/527336/do-we-need-asimovs-laws/',
-        summary: 'As robots become ever more present in daily life, the question of how to control their behaviour naturally arises. Does Asimov have the answer?',
-        keywords: 'robotics, AI, Sci-Fi, Asimov',
-        text: ''
-      }
-
-      item_2 = {
-        id: '2',
-        title: "xkcd: The Three Laws of Robotics",
-        link: 'https://xkcd.com/1613/',
-        summary: 'In ordering #5, self-driving cars will happily drive you around, but if you tell them to drive to a car dealership, they just lock the doors and politely ask how long humans take to starve to death.',
-        keywords: 'robotics, comics, Asimov, xkcd',
-        text: ''
-      }
-
-      item_3 = {
-        id: '3',
-        title: "The Myth of the Three Laws of Robotics - Why We Can't Control Intelligence",
-        link: 'https://singularityhub.com/2011/05/10/the-myth-of-the-three-laws-of-robotics-why-we-cant-control-intelligence/',
-        summary: 'From 1929 until the mid 90s, the author created many lasting tropes and philosophies that would define scifi for generations, but perhaps his most famous creation was the Three Laws of Robotics.',
-        keywords: 'future, Isaac Asimov, machine intelligence, opinion, Singularity, Three Laws of Robotics',
-        text: ''
-      }
-
-      item_4 = {
-        id: '4',
-        title: "The dawn of artificial intelligence",
-        link: 'http://www.economist.com/news/leaders/21650543-powerful-computers-will-reshape-humanitys-future-how-ensure-promise-outweighs',
-        summary: "Powerful computers will reshape humanity's future. How to ensure the promise outweighs the perils",
-        keywords: 'future, Clever computers, machine intelligence, opinion',
-        text: ''
-      }
-
-      item_5 = {
-        id: '5',
-        title: "Paul Allen: The Singularity Isn't Near",
-        link: 'https://www.technologyreview.com/s/425733/paul-allen-the-singularity-isnt-near/',
-        summary: 'The Singularity Summit approaches this weekend in New York. But the Microsoft cofounder and a colleague say the singularity itself is a long way off.',
-        keywords: 'future, robotics,machine intelligence,opinion',
-        text: ''
-      }
-
-      items << item_1
-      items << item_2
-      items << item_3
-      items << item_4
-      items << item_5
-
-      return items
-=end
-
-
-  # like/dislike and clicked with user saved both in rails database and recommendation database
-  # like => 1
-  # dislike => 0
 
   def self.clicked(content_id, user_id)
 
